5fd691c4b3aff1bb825b9db1cd050464c6785b8b
MosinWarrior <mykolasmazeikis22@gmail.com>
2025-03-03 01:28:08 +0200

    Pridėtas pause menu UI funkcionalumas
    ESC, kad iškviest pause menu

---
+++ PLACEHOLDER NAME/Assets/Scripts/PauseMenu/PauseMenuManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuManager : MonoBehaviour
+{
+
+    public void Resume()
+    {
+        Debug.Log("Resume button: Resume()");
+        MenuListener menuListener = FindObjectOfType<MenuListener>();
+        if (menuListener != null)
+        {
+            menuListener.ResumeGame();
+        }
+    }
+    public void QuitToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(2); //Scene doesn't exist yet
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
