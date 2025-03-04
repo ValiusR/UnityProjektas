a4581f4876fee5a573b93060fec08a924312d88f
Juozas <juozask18@gmail.com>
2025-03-04 21:59:02 +0200

    Pridėtas priešas

---
+++ PLACEHOLDER NAME/Assets/Scripts/Weapons/FireBallController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallController : WeaponController
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    
+
+    protected override void Attack()
+    {
+        base.Attack();
+
+        GameObject wpn = Instantiate(prefab);
+        wpn.transform.position = gameObject.transform.position;
+    }
+}
---
+++ PLACEHOLDER NAME/Assets/Scripts/Weapons/WeaponController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public GameObject prefab;
+
+    [Range(0f, 100f)]
+    [SerializeField] int damage;
+
+    [Range(0f, 100f)]
+    [SerializeField] float speed;
+
+    [Range(0f, 15)]
+    [SerializeField] float shootCooldown;
+
+    private float currCooldown;
+
+
+    protected virtual void Start()
+    {
+        currCooldown = 0;
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        currCooldown -= Time.deltaTime;
+
+        if (currCooldown <= 0 && Input.GetKey(KeyCode.Mouse0))
+        {
+            Attack();
+        }
+    }
+
+    protected virtual void Attack()
+    {
+        currCooldown = shootCooldown;
+    }
+
+
+}
