4c6e9d2917d57cf036b7d72bbcb0792131d7f47f
MosinWarrior <mykolasmazeikis22@gmail.com>
2025-03-31 17:45:04 +0300

    Sutvarkytras skriptų reference

--- PLACEHOLDER NAME/Assets/Tests/NewTestScript.cs
+++ PLACEHOLDER NAME/Assets/Tests/NewTestScript.cs
@@ -4,19 +4,21 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-public class TestScript
+public class NewTestScript
 {
+
     // A Test behaves as an ordinary method
     [Test]
-    public void TestScriptSimplePasses()
+    public void NewTestScriptSimplePasses()
     {
+       
         // Use the Assert class to test conditions
     }
 
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator TestScriptWithEnumeratorPasses()
+    public IEnumerator NewTestScriptWithEnumeratorPasses()
     {
         // Use the Assert class to test conditions.
         // Use yield to skip a frame.
