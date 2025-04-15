b18130b27d1745ee6cddf66070c7e6abfd0bf0df
Juozas <juozask18@gmail.com>
2025-04-15 18:09:17 +0300

    Boso atsiradimas

---
+++ PLACEHOLDER NAME/Assets/Scripts/Enemy/BossSpawner.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject bossPrefab;
+    [SerializeField] private float spawnTime = 60f; // Time in seconds
+    [SerializeField] private float spawnDistance = 5f; // Distance from player
+    [SerializeField] private Transform player;
+
+    private float timer;
+    private bool bossSpawned = false;
+
+    void Update()
+    {
+        if (bossSpawned) return;
+
+        timer += Time.deltaTime;
+
+        if (timer >= spawnTime)
+        {
+            SpawnBoss();
+            bossSpawned = true;
+        }
+    }
+
+    void SpawnBoss()
+    {
+        if (bossPrefab != null && player != null)
+        {
+            Vector2 randomOffset = Random.insideUnitCircle.normalized * spawnDistance;
+            Vector3 spawnPosition = player.position + new Vector3(randomOffset.x, randomOffset.y, 0);
+
+            Instantiate(bossPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+}
